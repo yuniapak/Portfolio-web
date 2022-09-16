5ac80156f02584ea081b61a6ccc64a00678317b2
Yun <yuniya.pa98@gmail.com>
2022-09-16 15:38:15 -0400

    deploy 10

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer =() =>{
     return (
         <div className='footer'>
             <a href='https://www.linkedin.com/in/iuniia-pakhomova/'>
-                <img src={Linkedin}/></a>
-            <a href='https://github.com/yuniapak'>
-                <img src={Git}/>
+                <img src='../assets/linkedin.png' alt='linkedin'/></a>
+            <a href='https://github.com/yuniapak' alt='github'>
+                <img src='../assets/github.svg'/>
             </a>
             <p>Icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
         </div>
